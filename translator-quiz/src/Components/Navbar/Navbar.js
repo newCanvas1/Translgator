63ac8e4afd3b30b28dce294f65dfc1fc0b5b40ee
newCanvas1 <123483334+newCanvas1@users.noreply.github.com>
2023-01-26 15:16:45 +0300

    Navbar addition

---
+++ translator-quiz/src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="brand">
+      <div class="icons8-google-translate"></div>
+        <p>Translator</p>
+      </div>
+      <p className="title">Challenge yourself !</p>
+    </nav>
+  );
+}
